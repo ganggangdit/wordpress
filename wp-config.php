ea84a8e5874781638ac5cdbc02c2afbf47b99555
ganggangdit <1158303912@qq.com>
2016-08-11 09:28:01 +0000

    wordpress

---
+++ wp-config.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ * The base configuration for WordPress
+ *
+ * The wp-config.php creation script uses this file during the
+ * installation. You don't have to use the web site, you can
+ * copy this file to "wp-config.php" and fill in the values.
+ *
+ * This file contains the following configurations:
+ *
+ * * MySQL settings
+ * * Secret keys
+ * * Database table prefix
+ * * ABSPATH
+ *
+ * @link https://codex.wordpress.org/Editing_wp-config.php
+ *
+ * @package WordPress
+ */
+
+// ** MySQL settings - You can get this info from your web host ** //
+/** The name of the database for WordPress */
+define('DB_NAME', 'bitnami_wordpress');
+
+/** MySQL database username */
+define('DB_USER', 'bn_wordpress');
+
+/** MySQL database password */
+define('DB_PASSWORD', '40a5b7fb73');
+
+/** MySQL hostname */
+define('DB_HOST', 'localhost:3306');
+
+/** Database Charset to use in creating database tables. */
+define('DB_CHARSET', 'utf8');
+
+/** The Database Collate type. Don't change this if in doubt. */
+define('DB_COLLATE', '');
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY', '8ed56c54c599191bd138065aba3aec8e621ed3abfe3f213e20817472b2095618');
+define('SECURE_AUTH_KEY', 'ee819de348a95ff25f2ea42fe6d8298d67e9aa5d484985938ab19d3da89c1b9a');
+define('LOGGED_IN_KEY', '70fd00940a281017a5b36cac8c7e5cc0ff9afc486f3625f56fd373fa3e695c3d');
+define('NONCE_KEY', '5b62751f49e3d998eb4cd578ab543721974c9bf41d177f58f00c7bef99f44df0');
+define('AUTH_SALT', '9a76e18bc778aeb1fd18c74396c54425e5524aa165dfed6a4738a9a4e98e8464');
+define('SECURE_AUTH_SALT', 'd01cea9c82f73fdeea665f8da7570d432912db1c4e1ccfc370bc43840c814972');
+define('LOGGED_IN_SALT', '68e2362413206d4a00ec5345be595f202d74716ecb5672de29bfa1d0ae5924af');
+define('NONCE_SALT', '7b3f7e68c3069d6c82a6123087ab34723c90fb294939120c52828a61727bcb71');
+
+/**#@-*/
+
+/**
+ * WordPress Database Table prefix.
+ *
+ * You can have multiple installations in one database if you give each
+ * a unique prefix. Only numbers, letters, and underscores please!
+ */
+$table_prefix  = 'wp_';
+
+/**
+ * For developers: WordPress debugging mode.
+ *
+ * Change this to true to enable the display of notices during development.
+ * It is strongly recommended that plugin and theme developers use WP_DEBUG
+ * in their development environments.
+ *
+ * For information on other constants that can be used for debugging,
+ * visit the Codex.
+ *
+ * @link https://codex.wordpress.org/Debugging_in_WordPress
+ */
+define('WP_DEBUG', false);
+
+/* That's all, stop editing! Happy blogging. */
+/**
+ * The WP_SITEURL and WP_HOME options are configured to access from any hostname or IP address.
+ * If you want to access only from an specific domain, you can modify them. For example:
+ *  define('WP_HOME','http://example.com');
+ *  define('WP_SITEURL','http://example.com');
+ *
+*/
+
+define('WP_SITEURL', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+define('WP_HOME', 'http://' . $_SERVER['HTTP_HOST'] . '/');
+
+
+/** Absolute path to the WordPress directory. */
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+
+/** Sets up WordPress vars and included files. */
+require_once(ABSPATH . 'wp-settings.php');
+
+define('WP_TEMP_DIR', '/opt/bitnami/apps/wordpress/tmp');
+
+
+define('FS_METHOD', 'ftpext');
+define('FTP_BASE', '/opt/bitnami/apps/wordpress/htdocs/');
+define('FTP_USER', 'bitnamiftp');
+define('FTP_PASS', 'AQk0HJ8UYIGpl9klQvwv7shXKGAlxDjU7RNJGyHqwW3uxqf32u');
+define('FTP_HOST', '127.0.0.1');
+define('FTP_SSL', false);
+
+
+
+//  Disable pingback.ping xmlrpc method to prevent Wordpress from participating in DDoS attacks
+//  More info at: https://wiki.bitnami.com/Applications/Bitnami_Wordpress#XMLRPC_and_Pingback
+
+// remove x-pingback HTTP header
+add_filter('wp_headers', function($headers) {
+    unset($headers['X-Pingback']);
+    return $headers;
+});
+// disable pingbacks
+add_filter( 'xmlrpc_methods', function( $methods ) {
+        unset( $methods['pingback.ping'] );
+        return $methods;
+});
