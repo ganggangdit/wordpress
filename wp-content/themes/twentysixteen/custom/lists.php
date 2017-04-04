bc6b3c23de78d482c6e466fdad29c05308d16c0c
wang.gang <wang.gang@immomo.com>
2017-04-04 20:51:57 +0800

    test

--- wp-content/themes/twentysixteen/custom/lists.php
+++ wp-content/themes/twentysixteen/custom/lists.php
@@ -5,8 +5,10 @@
  * @since  2017-04-04
  */
 
-print_r($wpdb) ;
-
+define("PATH", dirname(dirname(dirname(dirname(__FILE__)))).'/');
+require_once(PATH . "../wp-blog-header.php");
+global $wpdb;
+print_r($wpdb);
 print_r($_GET['cat']);
 
 
