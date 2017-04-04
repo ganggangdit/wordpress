dcd81203bb344d20c9ce7fae4895f31a590127d3
wang.gang <wang.gang@immomo.com>
2017-04-04 21:55:01 +0800

    test

--- wp-content/themes/twentysixteen/custom/lists.php
+++ wp-content/themes/twentysixteen/custom/lists.php
@@ -14,8 +14,10 @@ $data_array = array(
     'user_name'    => $_GET['name'],
     'user_message' => $_GET['message'],
 );
-$wpdb->insert($table, $data_array);
+//$wpdb->insert($table, $data_array);
 
-$sql     = $wpdb->prepare("SELECT user_name , user_message, FROM wp_weixin_stricky_note limit %s, %s", 0, 100);
+
+$sql = "SELECT user_name , user_message, FROM wp_weixin_stricky_note LIMIT 0,100";
+
 $results = $wpdb->get_results($sql);
 print_r($results);
